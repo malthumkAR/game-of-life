c85d4bdc93fb4878e2e658e38fc8e56c2bb1bba2
malthumkar <vinod9589@gmail.com>
2020-09-07 12:16:06 +0000

    hfhgd

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 # This is to demo
 
-node_app=`docker ps -a | grep nodeapp | awk '{print $NF}'`
+node_app=`docker ps -a | grep appnode | awk '{print $NF}'`
 if [ $node_app=='nodeapp' ]; then
     echo "nodeapp is running, lets delete"
-        docker rm -f nodeapp
+        docker rm -f appnode
 fi
 
 images=`docker images | grep vinod9782/ola | awk '{print $3}'`
 docker rmi $images
-docker run -d -p 8080:8080 --name nodeapp $1
+docker run -d -p 8080:8080 --name appnode $1
