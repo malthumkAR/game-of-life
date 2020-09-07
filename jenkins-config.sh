4d6f489d8dc5cda2aee55095475d67a3cb760da4
root <root@ip-172-31-45-255.us-east-2.compute.internal>
2020-09-07 08:51:47 +0000

    dfsaf

--- jenkins-config.sh
+++ jenkins-config.sh
@@ -1,14 +1,14 @@
-app_tag=`git ls-remote https://github.com/javahometech/node-app HEAD | awk '{print $1}'`
+app_tag=`git ls-remote https://github.com/malthumkAR/game-of-life.git HEAD | awk '{print $1}'`
 
-docker_app="kammana/nodejenkins:$app_tag"
+docker_app="vinod9782/ola:$app_tag"
 docker build -t $docker_app .
 
-docker login -u kammana -p yourpassword 
+docker login -u vinod9782 -p yourpassword 
 
 docker push $docker_app
 
-scp -i /var/lib/jenkins/dev.pem deploy.sh ec2-user@172.31.43.90:/tmp
+scp -i /var/lib/jenkins/dev.pem deploy.sh root@:172.31.45.255/tmp
 
-ssh -i /var/lib/jenkins/dev.pem ec2-user@172.31.43.90 chmod +x /tmp/deploy.sh
+ssh -i /var/lib/jenkins/dev.pem root@172.31.45.255 chmod +x /tmp/deploy.sh
 
-ssh -i /var/lib/jenkins/dev.pem ec2-user@172.31.43.90 /tmp/deploy.sh $docker_app
+ssh -i /var/lib/jenkins/dev.pem root@172.31.45.255 /tmp/deploy.sh $docker_app
